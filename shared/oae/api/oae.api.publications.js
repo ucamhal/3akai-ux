6d2d7c622a4f44d65eae0960603e43d019028d3a
Hal Blackburn <hwtb2@caret.cam.ac.uk>
2014-01-13 15:50:35 +0000

    Start of publications API client interface
    Only getting publications for a user is currently implemented.

---
+++ shared/oae/api/oae.api.publications.js
@@ -0,0 +1,49 @@
+/*!
+ * Copyright 2013 Apereo Foundation (AF) Licensed under the
+ * Educational Community License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ *     http://opensource.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+define(['exports', 'jquery'], function(exports, $) {
+
+    /**
+     * Get the publications authored by a user.
+     *
+     * @param  {String}         userId              The userId whose publications you wish to fetch
+     * @param  {String}         [start]             The token used for paging. If the first page of results is required, `null` should be passed in as the token. For any subsequent pages, the `nextToken` provided in the feed from the previous page should be used
+     * @param  {Number}         [limit]             Maximum number of publications to return
+     * @param  {Function}       callback            Standard callback method takes arguments `err` and `resp`
+     * @param  {Object}         callback.err        Error object containing error code and error message
+     * @param  {Object}         callback.response   The list of publications
+     */
+    var getUserPublications = exports.getUserPublications = function(userId, start, limit, callback) {
+        if (!userId) {
+            throw new Error('A valid user id should be provided');
+        }
+
+        var data = {
+            'start': start,
+            'limit': limit
+        };
+
+        $.ajax({
+            'url': '/api/publications/library/' + userId,
+            'data': data,
+            'success': function(data) {
+                callback(null, data);
+            },
+            'error': function(jqXHR, textStatus) {
+                callback({'code': jqXHR.status, 'msg': jqXHR.responseText});
+            }
+        });
+    };
+});
